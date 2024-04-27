01a8d15ca6b2e589cfb1eb0604326b6276632e23
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2024-04-26 17:48:31 -0700

    test

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from '@sveltejs/adapter-static';
 
 const config = {
-  kit: {
-      appDir: 'app', // Required as the default is _app
-      adapter: adapter()
-  },
-  paths: {
-    base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-  },
-  preprocess: vitePreprocess()
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter(),
+		paths: { base: '/Kanoodle-Solver' }
+	}
 };
 
 export default config;
